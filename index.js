aa9b942b5af8dc23b47c81539cc3c3921cbc6c5c
XuanSon99 <xuansonn12499@gmail.com>
2021-07-08 11:40:53 +0700

    chane

--- index.js
+++ index.js
@@ -4,72 +4,72 @@ function sleep(ms) {
 
 const post = async () => {
     // try {
-        let token = document.querySelector("#token").value
-        let group_list = document.querySelector("#group_list").value
-        let contents = document.querySelector("#content").value.split("|")
-        let image = document.querySelector("#image").value
-        let time = document.querySelector("#time").value
-        let type_via = document.querySelector("#type_via").value
+    let tokens = document.querySelector("#tokens").value.split("|")
+    let group_list = document.querySelector("#group_list").value
+    let contents = document.querySelector("#content").value.split("|")
+    let image = document.querySelector("#image").value
+    let time = document.querySelector("#time").value
+    // let type_via = document.querySelector("#type_via").value
 
-        let notifi = document.querySelector(".notification")
-        notifi.style.display = "block"
-        if (!time) {
-            message("error", "Vui lòng nhập thời gian chờ!")
-            return
+    let notifi = document.querySelector(".notification")
+    notifi.style.display = "block"
+    if (!time) {
+        message("error", "Vui lòng nhập thời gian chờ!")
+        return
+    }
+
+    // let tokens = await axios.get(
+    //     "https://sheets.googleapis.com/v4/spreadsheets/" + type_via + "/values/Worksheet!A1:A100?key=AIzaSyAAUda1-y7m8HYDOrDBr_-rqdMtf9TJZRI"
+    // ).then(res => {
+    //     return res.data.values
+    // })
+
+    let posts = await axios.get("https://graph.facebook.com/" + group_list + "/feed", {
+        params: {
+            access_token: tokens[0],
+            limit: 5
         }
+    }).then((res) => {
+        message("success", "Lấy ID 5 bài viết đầu thành công!")
+        return res.data.data
+    }).catch((error) => {
+        message("error", error.response.data.error.message)
+    })
 
-        let tokens = await axios.get(
-            "https://sheets.googleapis.com/v4/spreadsheets/" + type_via + "/values/Worksheet!A1:A100?key=AIzaSyAAUda1-y7m8HYDOrDBr_-rqdMtf9TJZRI"
-        ).then(res => {
-            return res.data.values
-        })
+    let post_list = []
+    for (let item of posts) {
+        post_list.push(item.id.split("_")[1])
+    }
 
-        let posts = await axios.get("https://graph.facebook.com/" + group_list + "/feed", {
-            params: {
+    await sleep(1000)
+
+    for (let token of tokens) {
+        for (let item of post_list) {
+            message("start", "Bắt đầu comment: " + item)
+
+            content = contents[Math.floor(Math.random() * contents.length)]
+
+            axios.post("https://graph.facebook.com/" + item + "/comments", {
                 access_token: token,
-                limit: 5
-            }
-        }).then((res) => {
-            message("success", "Lấy ID 5 bài viết đầu thành công!")
-            return res.data.data
-        }).catch((error) => {
-            message("error", error.response.data.error.message)
-        })
-
-        let post_list = []
-        for (let item of posts) {
-            post_list.push(item.id.split("_")[1])
+                message: content,
+                attachment_url: image
+            }).then((res) => {
+                message("success", "Comment thành công")
+            }).catch((error) => {
+                message("error", error.response.data.error.message)
+            })
+            let t = time
+            let cowndown = setInterval(() => {
+                t--
+                document.querySelector(".notification span").textContent = t + "s"
+                if (t == 0) {
+                    clearInterval(cowndown)
+                    document.querySelector(".notification span").textContent = "Ok"
+                }
+            }, 1000);
+            await sleep(time * 1000)
         }
-
-        await sleep(1000)
-
-        for (let token of tokens) {
-            for (let item of post_list) {
-                message("start", "Bắt đầu comment: " + item)
-
-                content = contents[Math.floor(Math.random() * contents.length)]
-
-                axios.post("https://graph.facebook.com/" + item + "/comments", {
-                    access_token: token[0],
-                    message: content,
-                    attachment_url: image
-                }).then((res) => {
-                    message("success", "Comment thành công")
-                }).catch((error) => {
-                    // message("error", error.response.data.error.message)
-                })
-                let t = time
-                let cowndown = setInterval(() => {
-                    t--
-                    document.querySelector(".notification span").textContent = t + "s"
-                    if (t == 0) {
-                        clearInterval(cowndown)
-                        document.querySelector(".notification span").textContent = "Ok"
-                    }
-                }, 1000);
-                await sleep(time * 1000)
-            }
-        }
+    }
     // } catch (error) {
     //     if (confirm("Click oki to see the tutorial")) {
     //         window.open("https://docs.google.com/document/d/1PW91i6qqhUh-b2T6AvBl9CmD21BBNHU4JojPXhFvmt0/edit?usp=sharing")
